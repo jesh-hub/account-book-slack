30c6153f2b014d5c0748947cab4e640320712cab
Sang-hyun, Cho <cho911115@gmail.com>
2022-04-12 01:13:58 +0900

    refactor: serverless에서 분리한 패키지 import (#9)

--- back-end/sls/go.mod
+++ back-end/sls/go.mod
@@ -2,6 +2,7 @@ module github.com/jesh-hub/account-book-slack/back-end
 
 go 1.18
 
-require github.com/aws/aws-lambda-go v1.6.0
-
-require github.com/stretchr/testify v1.7.1 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.6.0
+	github.com/jesh-hub/account-book-slack/back-end/pkg/abs v0.0.0-20220411155844-11c990cd49ac
+)
--- back-end/sls/payments/main.go
+++ back-end/sls/payments/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"log"
-	"net/http"
-	"os"
-	"time"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/jesh-hub/account-book-slack/back-end/pkg/abs"
+	"log"
+	"os"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -20,22 +18,15 @@ type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context) (Response, error) {
-	// 슬랙에서 채팅 내역 가져오기
-	slackClient := NewSlackClient()
+	slackToken := os.Getenv("slackToken")
+	channelId := os.Getenv("channelId")
+	botId := os.Getenv("botId")
+
+	slackClient := abs.NewSlackClient(slackToken, channelId, botId)
 	messages := slackClient.GetMessages()
+	messages = slackClient.FilterMessages(messages)
 
-	// 채팅 내역을 []Payment로 데이터 가공
-	var payments []Payment
-	for _, message := range messages {
-		if len(message.ClientMsgID) > 0 {
-			payments = append(payments, Payment{
-				Date: message.Text,
-			})
-		}
-	}
-
-	// 가공한 []Payment 리턴
-	body, err := json.Marshal(payments)
+	body, err := json.Marshal(messages)
 	if err != nil {
 		return Response{StatusCode: 404}, err
 	}
@@ -51,65 +42,6 @@ func Handler(ctx context.Context) (Response, error) {
 	}
 
 	return resp, nil
-}
-
-type SlackHistory struct {
-	Messages []Message `json:"messages"`
-}
-
-type Message struct {
-	ClientMsgID string `json:"client_msg_id,omitempty"`
-	Text        string `json:"text"`
-	Ts          string `json:"ts"`
-}
-
-type Payment struct {
-	Date               string `json:"date"`
-	Name               string `json:"name"`
-	Category           string `json:"category"`
-	Method             string `json:"method"`
-	Price              int    `json:"price"`
-	MonthlyInstallment int    `json:"monthlyInstallment"`
-}
-
-type SlackClient struct {
-	token      string
-	channelId  string
-	httpClient *http.Client
-}
-
-func NewSlackClient() *SlackClient {
-	return &SlackClient{
-		token:     os.Getenv("slackToken"),
-		channelId: os.Getenv("channelId"),
-		httpClient: &http.Client{
-			Timeout: 5 * time.Second,
-		},
-	}
-}
-
-func (s *SlackClient) GetMessages() []Message {
-	req, err := http.NewRequest("GET", "https://slack.com/api/conversations.history", nil)
-	errorHandler(err)
-
-	// 헤더
-	req.Header.Add("Authorization", "Bearer "+s.token)
-
-	// urlQeury
-	q := req.URL.Query()
-	q.Add("channel", s.channelId)
-	req.URL.RawQuery = q.Encode()
-
-	resp, err := s.httpClient.Do(req)
-	errorHandler(err)
-
-	defer resp.Body.Close()
-
-	slackHistory := SlackHistory{}
-	err = json.NewDecoder(resp.Body).Decode(&slackHistory)
-	errorHandler(err)
-
-	return slackHistory.Messages
 }
 
 func errorHandler(err error) {
