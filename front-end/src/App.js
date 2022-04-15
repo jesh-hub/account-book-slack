1f4bf0d878db1e87679f0d03493e123abffdb6f5
Jeong-Eun, Kim <jee824k@gmail.com>
2022-04-16 02:38:25 +0900

    feat: 헤더에 월 선택 기능 추가

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import {DropdownButton, Dropdown} from 'react-bootstrap';
+import {useState} from 'react';
 
 function App() {
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+
+  const monthDropdownItems = [];
+  for (let i = 0; i < 12; i++)
+    monthDropdownItems.push(
+      <Dropdown.Item
+        key={i}
+        disabled={currentMonth === i}
+        onClick={() => setCurrentMonth(i)}
+      >
+        {i + 1}월
+      </Dropdown.Item>);
+
   return (
     <div className="app">
       <header className="app-header">
+        <DropdownButton
+          className="abs-full-width-button"
+          variant="outline-primary"
+          title={`${currentMonth + 1}월`}
+        >
+          {monthDropdownItems}
+        </DropdownButton>
       </header>
     </div>
   );
