6ce9a1f7faa0bbcb7dc081f73566fc66b8e760b6
Sang-hyun, Cho <cho911115@gmail.com>
2022-07-26 23:55:13 +0900

    refactor: API URI에서 도메인 뒤에 붙는 /abs 제거

--- back-end/router/router.go
+++ back-end/router/router.go
@@ -5,10 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const URL_PREFIX = "/abs"
-
 func NewAbsRouterV1(r *gin.Engine) *gin.RouterGroup {
-	v1 := r.Group(URL_PREFIX + "/v1")
+	v1 := r.Group("/v1")
 	{
 		v1.POST("/login", controller.Login)
 		// group
