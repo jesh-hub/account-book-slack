ccc7401663e983b3aa30b22ae46d1d47440ceda0
Sang-hyun, Cho <cho911115@gmail.com>
2022-07-27 23:53:58 +0900

    fix: OPTIONS 요청에 대한 처리가 없어 CORS 에러 발생

--- back-end/middleware/middleware.go
+++ back-end/middleware/middleware.go
@@ -10,5 +10,9 @@ var allowOrigin = util.GodotEnv("ALLOW_ORIGIN")
 func SetHeader(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", allowOrigin)
 	c.Header("Cache-Control", "no-store")
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
 	c.Next()
 }
