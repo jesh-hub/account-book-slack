dd497145a87ea23c48715b91005d80bb04f35e9e
Jeong-eun, Kim <jee824k@gmail.com>
2022-08-21 01:39:47 +0900

    결제 수단까지 등록할 수 있게 했다.
    * back-end에서 paymentMethod에 groupId를 required로 표시해서 건의해둔 상태

--- front-end/src/pages/GroupRegisterView.jsx
+++ front-end/src/pages/GroupRegisterView.jsx
@@ -4,13 +4,20 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+const BadgeBg = ['info', 'light'];
+const BadgeText = [undefined, 'dark'];
+
 export default function GroupRegisterView(props) {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: '',
-    emailToInvite: '',
+    email: '',
+    paymentMethod: '',
   });
-  const [invitedEmails, setInvitedEmails] = useState([]);
+  const [apiData, setApiData] = useState({
+    email: [],
+    paymentMethod: []
+  });
 
   function handleFormDataChanged(evt) {
     const { name, value } = evt.target;
@@ -20,23 +27,27 @@ export default function GroupRegisterView(props) {
     }));
   }
 
-  function addInvitedEmail() {
-    if (invitedEmails.includes(formData.emailToInvite))
+  function handleApiDataChanged(name) {
+    if (apiData[name].includes(formData[name]))
       return;
-    setInvitedEmails(invitedEmails => invitedEmails.concat(formData.emailToInvite));
+    setApiData(apiData => ({
+      ...apiData,
+      [name]: apiData[name].concat(formData[name])
+    }));
     setFormData(formData => ({
       ...formData,
-      emailToInvite: '',
+      [name]: '',
     }));
   }
 
-  function removeInvitedEmail(invitedEmail) {
-    const index = invitedEmails.indexOf(invitedEmail);
+  function removeApiData(name, value) {
+    const index = apiData[name].indexOf(value);
     if (index < 0)
       return;
-    const _invitedEmails = [...invitedEmails];
-    _invitedEmails.splice(index, 1);
-    setInvitedEmails(_invitedEmails);
+    setApiData(apiData => ({
+      ...apiData,
+      [name]: apiData[name].filter(data => data !== value)
+    }));
   }
 
   async function submit(evt) {
@@ -44,9 +55,10 @@ export default function GroupRegisterView(props) {
     try {
       await axios.post('/v1/group', {
         name: formData.name,
-        users: [props.userInfo.email].concat(invitedEmails),
+        users: [props.userInfo.email].concat(apiData.email),
+        regUserId: props.userInfo.id,
         modUserId: props.userInfo.id,
-        paymentMethods: []
+        paymentMethod: apiData.paymentMethod
       });
       navigate(-1);
     } catch (e) {
@@ -70,13 +82,14 @@ export default function GroupRegisterView(props) {
             required
           />
         </Form.Group>
+        {/* 이 밑으로 두 개 중복 코드 */}
         <Form.Group className="register-row-group">
           <Form.Label>그룹 구성원 추가하기</Form.Label>
           <Row className="register-row">
             <Col
               as={Form.Control}
-              name="emailToInvite"
-              value={formData.emailToInvite}
+              name="email"
+              value={formData.email}
               placeholder="초대할 이메일"
               onChange={handleFormDataChanged}
             />
@@ -86,41 +99,72 @@ export default function GroupRegisterView(props) {
               variant="outline-primary"
               // TODO 이메일 형식에 맞지 않으면 disable
               // TODO 이미 추가한 이메일이면 disable
-              onClick={addInvitedEmail}
+              onClick={() => handleApiDataChanged('email')}
             >
               추가
             </Col>
           </Row>
           <ul className="invited-emails">
             {
-              invitedEmails.map((email, i) => {
-                let bg, text;
-                if (i % 2) {
-                  bg = 'light';
-                  text = 'dark';
-                } else
-                  bg = 'info';
-
-                return (
-                  <li
-                    key={email}
-                    className="li__email"
-                  >
-                    <Badge
-                      size="sm"
-                      bg={bg}
-                      text={text}
-                    >
-                      {email}
-                    </Badge>
-                    <Button
-                      size="xs"
-                      variant="soft-clear"
-                      onClick={() => removeInvitedEmail(email)}
-                    >x</Button>
-                  </li>
-                );
-              })
+              apiData.email.map((email, i) => (
+                <li
+                  key={email}
+                  className="li__email"
+                >
+                  <Badge
+                    size="sm"
+                    bg={BadgeBg[i % 2]}
+                    text={BadgeText[i % 2]}
+                  >{email}</Badge>
+                  <Button
+                    size="xs"
+                    variant="clear"
+                    onClick={() => removeApiData('email', email)}
+                  >x</Button>
+                </li>
+              ))
+            }
+          </ul>
+        </Form.Group>
+        <Form.Group className="register-row-group">
+          <Form.Label>결제 수단 추가하기</Form.Label>
+          <Row className="register-row">
+            <Col
+              as={Form.Control}
+              name="paymentMethod"
+              value={formData.paymentMethod}
+              placeholder="결제 수단 이름"
+              onChange={handleFormDataChanged}
+            />
+            <Col
+              as={Button}
+              className="col-3"
+              variant="outline-primary"
+              // TODO 이미 추가한 결제 수단이면 disable
+              onClick={() => handleApiDataChanged('paymentMethod')}
+            >
+              추가
+            </Col>
+          </Row>
+          <ul className="invited-emails">
+            {
+              apiData.paymentMethod.map((method, i) => (
+                <li
+                  key={method}
+                  className="li__email"
+                >
+                  <Badge
+                    size="sm"
+                    bg={BadgeBg[i % 2]}
+                    text={BadgeText[i % 2]}
+                  >{method}</Badge>
+                  <Button
+                    size="xs"
+                    variant="clear"
+                    onClick={() => removeApiData('paymentMethod', method)}
+                  >x</Button>
+                </li>
+              ))
             }
           </ul>
         </Form.Group>
