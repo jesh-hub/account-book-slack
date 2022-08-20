034988979fb8b4d7fca7f61ec0c64adaa2c1451c
Sang-hyun, Cho <cho911115@gmail.com>
2022-08-21 02:21:32 +0900

    refactor: 결제수단 등록 API의 파라미터를 entity에서 DTO로 변경 (mode.PaymentMethod -> mode.PaymentMethodAdd)

--- back-end/model/payment_method.go
+++ back-end/model/payment_method.go
@@ -7,12 +7,29 @@ import (
 
 type PaymentMethod struct {
 	mgm.DefaultModel `bson:",inline"`
-	Name             string             `json:"name" bson:"name" binding:"required"`
+	Name             string             `json:"name" bson:"name"`
 	Default          bool               `json:"default" bson:"default"`
-	GroupId          primitive.ObjectID `json:"groupId" bson:"groupId" binding:"required"`
+	GroupId          primitive.ObjectID `json:"groupId" bson:"groupId"`
+}
+
+type PaymentMethodAdd struct {
+	Name    string `json:"name" bson:"name" binding:"required"`
+	Default bool   `json:"default" bson:"default"`
+}
+
+func (pma *PaymentMethodAdd) ToEntity() *PaymentMethod {
+	return &PaymentMethod{
+		Name:    pma.Name,
+		Default: pma.Default,
+	}
 }
 
 type PaymentMethodUpdate struct {
 	Name    string `json:"name" bson:"name" binding:"required"`
 	Default bool   `json:"default" bson:"default"`
+}
+
+func (pmu *PaymentMethodUpdate) ToEntity(paymentMethod *PaymentMethod) {
+	paymentMethod.Name = pmu.Name
+	paymentMethod.Default = pmu.Default
 }
