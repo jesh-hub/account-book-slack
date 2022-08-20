ec79e4d87dcbe219bc587c7825afd5d896131399
Sang-hyun, Cho <cho911115@gmail.com>
2022-08-21 02:22:38 +0900

    refactor: 그룹 등록 API의 파라미터를 entity에서 DTO로 변경 (mode.group -> mode.groupAdd)

--- back-end/model/group.go
+++ back-end/model/group.go
@@ -4,11 +4,26 @@ import "github.com/kamva/mgm/v3"
 
 type Group struct {
 	mgm.DefaultModel `bson:",inline"`
-	Name             string           `json:"name" bson:"name" binding:"required"`
-	Users            []string         `json:"users" bson:"users" binding:"required"`
+	Name             string           `json:"name" bson:"name"`
+	Users            []string         `json:"users" bson:"users"`
 	RegUserId        string           `json:"regUserId" bson:"regUserId"`
 	ModUserId        string           `json:"modUserId" bson:"modUserId"`
 	PaymentMethods   *[]PaymentMethod `json:"paymentMethods,omitempty" bson:"paymentMethods,omitempty"`
+}
+
+type GroupAdd struct {
+	Name             string              `json:"name" bson:"name" binding:"required"`
+	Users            []string            `json:"users" bson:"users" binding:"required"`
+	RegUserId        string              `json:"regUserId" bson:"regUserId" binding:"required"`
+	PaymentMethodAdd *[]PaymentMethodAdd `json:"paymentMethodAdd,omitempty" bson:"paymentMethodAdd,omitempty"`
+}
+
+func (ga *GroupAdd) ToEntity() *Group {
+	return &Group{
+		Name:      ga.Name,
+		Users:     ga.Users,
+		RegUserId: ga.RegUserId,
+	}
 }
 
 type GroupFind struct {
@@ -21,4 +36,10 @@ type GroupUpdate struct {
 	Name      string   `json:"name" bson:"name" binding:"required"`
 	Users     []string `json:"users" bson:"users" binding:"required"`
 	ModUserId string   `json:"modUserId" bson:"modUserId" binding:"required"`
+}
+
+func (gu *GroupUpdate) ToEntity(group *Group) {
+	group.Name = gu.Name
+	group.Users = gu.Users
+	group.ModUserId = gu.ModUserId
 }
